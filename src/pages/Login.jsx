4a17759ca47d988e2f3e932332016b0a25e863bc
Alexander <alexadedeji15@gmail.com>
2022-08-16 01:49:52 +0100

    login design done

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { userLogin } from "../utils/apis";
 import { setAccessToken } from "../utils/services/apiAdapter";
-import {toast} from "react-toastify"
+import { toast } from "react-toastify";
 const Login = () => {
   const navigate = useNavigate();
   const [loginDetails, setLoginDetails] = useState({
@@ -22,7 +22,8 @@ const Login = () => {
   const navigateHandler = useCallback(() => {
     navigate("/home");
   }, [navigate]);
-  const onSubmit = async (arams) => {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
     console.log(loginDetails);
     try {
       const { data } = await userLogin(loginDetails);
@@ -32,22 +33,51 @@ const Login = () => {
       localStorage.setItem("userId", data.results.user_id);
       navigateHandler();
     } catch (error) {
-        toast.error(error?.response?.data?.error_description || "An error occured")
+      toast.error(
+        error?.response?.data?.error_description || "An error occured"
+      );
     }
   };
   return (
     <div>
-      <input
-        placeholder="email"
-        name="email"
-        onChange={(e) => onChangeHandler(e)}
-      />
-      <input
-        placeholder="password"
-        name="password"
-        onChange={(e) => onChangeHandler(e)}
-      />
-      <button onClick={onSubmit}>Submit</button>
+      <div class="parent clearfix">
+        <div class="bg-illustration">
+          <img src="https://i.ibb.co/Pcg0Pk1/logo.png" alt="logo" />
+
+          <div class="burger-btn">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </div>
+
+        <div class="login">
+          <div class="container">
+            <h1>
+              Login to access to
+              <br />
+              your account
+            </h1>
+
+            <div class="login-form">
+              <form onSubmit={handleSubmit}>
+                <input
+                  placeholder="email"
+                  name="email"
+                  onChange={(e) => onChangeHandler(e)}
+                />
+                <input
+                  placeholder="password"
+                  name="password"
+                  onChange={(e) => onChangeHandler(e)}
+                />
+
+                <button type="submit">Log In</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
